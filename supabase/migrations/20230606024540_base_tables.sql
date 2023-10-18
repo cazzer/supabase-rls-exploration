091b269b7b07978db1f59c90d0f47c7241eca12d
Caleb Brewer <caleb@cazzer.io>
2023-10-17 22:56:54 -0700

    formatting sql and dropping deferrable

--- supabase/migrations/20230606024540_base_tables.sql
+++ supabase/migrations/20230606024540_base_tables.sql
@@ -1,80 +1,82 @@
 create extension if not exists "uuid-ossp";
 
-CREATE TABLE items (
-    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+create TABLE
+  items (
+    id uuid default gen_random_uuid () primary key,
     content text,
-    created_at timestamp with time zone NOT NULL DEFAULT now(),
-    public boolean NOT NULL DEFAULT false,
-    metadata jsonb DEFAULT '{}'::jsonb
-);
+    created_at timestamp with time zone not null default now(),
+    public boolean not null default false,
+    metadata jsonb default '{}'::jsonb
+  );
+
 alter table items enable row level security;
-alter publication supabase_realtime add table items;
 
-CREATE TABLE item_permissions (
-    id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-    item_id uuid REFERENCES items(id) ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
-    permitted_id uuid NOT NULL
-);
+alter publication supabase_realtime
+add table items;
+
+create table
+  item_permissions (
+    id uuid default gen_random_uuid () primary key,
+    item_id uuid references items (id) on delete cascade,
+    permitted_id uuid not null
+  );
+
 alter table item_permissions enable row level security;
-alter publication supabase_realtime add table item_permissions;
 
-create policy insert_items
-on items
-for insert
-to authenticated
-with check (true);
+alter publication supabase_realtime
+add table item_permissions;
 
-CREATE OR REPLACE FUNCTION volatilePermissionCheck(item_id uuid) RETURNS TABLE (id uuid) AS $$
-    select id from item_permissions
-    where item_permissions.item_id = item_id
-    and permitted_id = auth.uid()
-$$ LANGUAGE sql VOLATILE;
+-- allow all authenticated users to insert items
+create policy insert_items on items for insert to authenticated
+with
+  check (true);
 
-CREATE POLICY select_item
-ON items
-FOR SELECT
-to authenticated
-USING (
-    NOT EXISTS (
-      select *
-      from item_permissions
-      where item_id = items.id
+-- allow all authenticated users to select items that they just created
+create policy select_item on items for
+select
+  to authenticated using (
+    not exists (
+      select
+        *
+      from
+        item_permissions
+      where
+        item_id = items.id
     )
-);
+  );
 
-create policy manage_item
-on items
-for all
-to authenticated
-using (
-  exists(
-    select item_id
-    from item_permissions
-    where items.id = item_id
-    and permitted_id = auth.uid()
+-- allow authenticated users to do anything to items they have permission for
+create policy manage_item on items for all to authenticated using (
+  exists (
+    select
+      item_id
+    from
+      item_permissions
+    where
+      items.id = item_id
+      and permitted_id = auth.uid ()
   )
 )
-with check (
-  exists(
-    select item_id
-    from item_permissions
-    where items.id = item_id
-    and permitted_id = auth.uid()
-  )
-);
+with
+  check (
+    exists (
+      select
+        item_id
+      from
+        item_permissions
+      where
+        items.id = item_id
+        and permitted_id = auth.uid ()
+    )
+  );
 
-create policy select_public_item
-on items
-for select
-to public
-using (
-  public = true
-);
+-- allow public users to select public items
+create policy select_public_item on items for
+select
+  to public using (public = true);
 
-create or replace function insert_permission()
-  returns trigger
-  security definer
-  as $$
+create
+or replace function insert_permission () returns trigger security definer as $$
 begin
   insert into item_permissions (item_id, permitted_id) values (
     new.id,
@@ -85,48 +87,28 @@ end
 $$ language plpgsql;
 
 create trigger insert_permission_trigger
-before insert
-on items
-for each row
-execute procedure insert_permission();
+after insert on items for each row
+execute procedure insert_permission ();
 
--- create policy insert_item_permission_for_new_item
--- on item_permissions
--- for insert
--- with check (
---   permitted_id = auth.uid()
---   and not exists(
---     select true
---     from item_permissions
---     where item_id = item_id
---   )
--- );
+-- allow all authenticated users to select item permissions
+create policy select_item_permissions on item_permissions for
+select
+  to authenticated using (true);
 
-create policy select_item_permissions
-on item_permissions
-for select
-to authenticated
-using (
-  true
-);
+-- allow all authenticated users to insert item permissions
+create policy insert_item_permissions on item_permissions for insert to authenticated
+with
+  check (
+    exists (
+      select
+        id
+      from
+        item_permissions
+      where
+        item_id = item_id
+        and permitted_id = auth.uid ()
+    )
+  );
 
-create policy insert_item_permissions
-on item_permissions
-for insert
-to authenticated
-with check (
-  exists (
-    select id
-    from item_permissions
-    where item_id = item_id
-    and permitted_id = auth.uid()
-  )
-);
-
-create policy delete_item_permissions
-on item_permissions
-for delete
-to authenticated
-using (
-  permitted_id = auth.uid()
-);
+-- allow all authenticated users to delete their own permissions
+create policy delete_item_permissions on item_permissions for delete to authenticated using (permitted_id = auth.uid ());
